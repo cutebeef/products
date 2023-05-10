b1bd5d75549de0669bd2b61ff8d780fe7ce64f6c
Ray Ray Liu <cutebeef@gmail.com>
2023-05-10 21:15:36 +0800

    update

--- products.py
+++ products.py
@@ -13,3 +13,8 @@ print(products)
 
 for product in products:
 	print(product[0], '的價格是', product[1])
+
+with open('product.csv', 'w') as f: #r讀取 w寫入
+	 for product in products:
+	 	f.write(product[0] + ',' + product[1] + '\n')
+	 	#open只是打開而已 下面的write才是真的寫入
